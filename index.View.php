1a4fa161f31d06f4118d09e519c0537cf5185f9e
frankpezo <73926318+frankpezo@users.noreply.github.com>
2023-04-10 17:27:29 -0500

    Diseño formulario

---
+++ index.View.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Formulario</title>
+</head>
+<body>
+     <div class="wrap">
+        <form action="">
+            <input type="text" name="nombre" id="" placeholder="Nombre" value="">
+            <input type="text" name="email" id=""  placeholder="Email" value"="">
+            <textarea name="mensaje" placeholder="Mensaje"></textarea>
+            <!-- mensaje de error-->
+            <div class="alert error">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+            </div>
+            <div class="alert success">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+            </div>
+            <input type="submit"  name="submit" value="Enviar">
+        </form>
+     </div>
+</body>
+</html>
